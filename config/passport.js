a678afa09f9d9ddbae4e90c5976725917e43a599
Allen Yen <nothing86617@gmail.com>
2024-08-19 14:29:50 +0800

    modify redirect address

--- config/passport.js
+++ config/passport.js
@@ -22,7 +22,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/auth/google/redirect",
+      callbackURL:
+        "https://google-login-ejz9.onrender.com//auth/google/redirect",
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log("進入Google Strategy的區域");
